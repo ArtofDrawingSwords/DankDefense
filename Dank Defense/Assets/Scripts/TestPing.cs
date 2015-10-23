d8fbc1e223cafc1e92021d30a0a55f1c215b65cd
ArtofDrawingSwords <brandonscoolletters@hotmail.com>
2015-10-22 23:19:46 -0700

    Very basic networking

---
+++ Dank Defense/Assets/Scripts/TestPing.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class TestPing : NetworkBehaviour {
+
+    private NetworkClient nClient;
+    private float latency;
+
+	// Use this for initialization
+	void Start () {
+        nClient = GameObject.Find("NetworkManager").GetComponent<NetworkManager>().client;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnGUI()
+    {
+        if(isLocalPlayer)
+        {
+            GUI.Label(new Rect(10, Screen.height - 50, 250, 50), nClient.GetRTT().ToString());
+        }
+    }
+
+    void showLatency()
+    {
+
+    }
+}
