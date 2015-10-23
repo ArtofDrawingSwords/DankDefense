f25c58b173fa173713e8b1e40539a2713b6df924
ArtofDrawingSwords <brandonscoolletters@hotmail.com>
2015-10-22 22:09:03 -0700

    player controller update

--- Dank Defense/Assets/Scripts/TestPlayerController.cs
+++ Dank Defense/Assets/Scripts/TestPlayerController.cs
@@ -4,15 +4,18 @@ using System.Collections;
 public class TestPlayerController : MonoBehaviour {
 
     public float speed = 1;
+    public float rigidBodySpeed = 100;
 
     float x;
     float y;
     Transform playerTransform;
+    Rigidbody2D playerRigidBody;
 
 	// Use this for initialization
 	void Start () {
         playerTransform = GetComponent<Transform>();
-	}
+        playerRigidBody = GetComponent<Rigidbody2D>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -22,6 +25,7 @@ public class TestPlayerController : MonoBehaviour {
 
     void FixedUpdate()
     {
-        transform.position += new Vector3(x * Time.deltaTime * speed, y * Time.deltaTime * speed, 0);
+        playerRigidBody.velocity = new Vector2(x * Time.deltaTime * rigidBodySpeed, y * Time.deltaTime * rigidBodySpeed);
+        //transform.position += new Vector3(x * Time.deltaTime * speed, y * Time.deltaTime * speed, 0);
     }
 }
