1751d72640466d8f4cb080c7834936af6ab639ef
ArtofDrawingSwords <brandonscoolletters@hotmail.com>
2015-10-22 21:24:22 -0700

    test playercontroller

---
+++ Dank Defense/Assets/Scripts/TestPlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestPlayerController : MonoBehaviour {
+
+    public float speed = 1;
+
+    float x;
+    float y;
+    Transform playerTransform;
+
+	// Use this for initialization
+	void Start () {
+        playerTransform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        x = Input.GetAxisRaw("Horizontal");
+        y = Input.GetAxisRaw("Vertical");
+	}
+
+    void FixedUpdate()
+    {
+        transform.position += new Vector3(x * Time.deltaTime * speed, y * Time.deltaTime * speed, 0);
+    }
+}
